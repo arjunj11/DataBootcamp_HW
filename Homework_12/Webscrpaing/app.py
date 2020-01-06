57070c77589884a915c65b3d64ce0ab0e64b7067
Arjun11 <arjun.johri7@gmail.com>
2020-01-06 15:44:36 -0500

    one

--- Homework_12/Webscrpaing/app.py
+++ Homework_12/Webscrpaing/app.py
@@ -9,20 +9,26 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route('/')
 def home(): 
+
+    import missionstomars
+    scraped_dict=missionstomars.scrape()
+
+
     # Find one record of data from the mongo database
-    mars_data = mongo.db.mars
+    #mars_data = mongo.db.mars.find()
 
     # Return template and data
-    return render_template("index.html", mrs=mars_data)
+    return render_template("index.html", mrs=scraped_dict)
+    print(mars_data)
 
 
 @app.route('/scrape')
 def scrapes():     
-    import missionstomars
-    scraped_dict=missionstomars.scrape()
+    # import missionstomars
+    # scraped_dict=missionstomars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars.update({}, scraped_dict, upsert=True)
+    #mongo.db.mars.update({}, scraped_dict, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
